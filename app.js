1498492e665bafe04c061dd76a19c5bd9ce3273e
Sebastian Górski <silent8668@gmail.com>
2019-07-31 14:30:33 +0200

    Update newgame event

--- app.js
+++ app.js
@@ -119,6 +119,7 @@ function init() {
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
+    document.querySelector('.final-score').value = 'none';
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
     document.querySelector('.player-0-panel').classList.remove('winner');
